e8feb97a38a3495a2516a54667c741508d1f7c6b
mabsattar <mabsattar@proton.me>
2025-03-02 03:23:14 +0530

    renamed file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");
+
 module.exports = {
-  solidity: "0.8.28",
+  defaultNetwork: "swisstronik",
+  solidity: "0.8.17",
+  networks: {
+    swisstronik: {
+      url: "httpsL//json-rpc.testnet.swisstronik.com/",
+      accounts: [`0xF2EAE37f128487021E535526ECc9b2Af3689c16F` + `${PRIVATE_KEY}`],
+    },
+  },
 };
