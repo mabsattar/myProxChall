5fa6215342ba0e43d59f655e801f55a2658b8598
mabsattar <mabsattar@proton.me>
2025-03-05 03:44:26 +0530

    modified account num, added sample contract and compiled contract

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,8 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "httpsL//json-rpc.testnet.swisstronik.com/",
-      accounts: [`0xF2EAE37f128487021E535526ECc9b2Af3689c16F` + `${PRIVATE_KEY}`],
+      accounts: [`0x` + `${PRIVATE_KEY}`],
+      chainId: 1291,
     },
   },
 };
